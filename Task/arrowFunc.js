ae6d50efe2f364eeee71ad11a918339933827325
Jeromel Pushparaj <jeromal03@gmail.com>
2024-04-13 22:09:20 +0530

    learned about the arrow function in the Javascript

---
+++ Task/arrowFunc.js
@@ -0,0 +1,16 @@
+let greet = (name) =>{
+    console.log("Welcome! " + name);
+    return 1;
+}
+
+console.log(greet("JOthi Jackulin"));
+
+let negToPos = (number) => {
+    if (number < 0){
+        number = number * -1;
+    }
+    console.log(number);
+    
+}
+
+negToPos(28);
