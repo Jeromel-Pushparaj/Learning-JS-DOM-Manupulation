614960a18902b67b827237ac7e74b2ac203b4433
Jeromel Pushparaj <jeromal03@gmail.com>
2024-04-21 16:45:46 +0530

    little modifications in rock paper scissors

--- Task/rps.js
+++ Task/rps.js
@@ -40,21 +40,28 @@ function playRound(playerSelection,computerSelection){
     }
 }
 
+const prompt = require('prompt-sync')({ sigint: true });
+let i = 0;
+while(i == 0){
+    let playerSelection = prompt("Enter you choice: ");
+    console.log("Player: " + playerSelection);
+    const computerSelection = getComputerChoice(randomInt);
+    console.log("Computer: "+ computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    i = prompt("continue = 0, exit = 1: ")
+}
 
-const playerSelection = "papper";
-console.log("Player: " + playerSelection);
-const computerSelection = getComputerChoice(randomInt);
-console.log("Computer: "+ computerSelection);
-console.log(playRound(playerSelection, computerSelection));
 
-const playerSelection1 = "scissors";
-console.log("Player: " + playerSelection1);
-const computerSelection1 = getComputerChoice(randomInt);
-console.log("Computer: "+ computerSelection1);
-console.log(playRound(playerSelection1, computerSelection1));
 
-const playerSelection2 = "rock";
-console.log("Player: " + playerSelection2);
-const computerSelection2 = getComputerChoice(randomInt);
-console.log("Computer: "+ computerSelection2);
-console.log(playRound(playerSelection2, computerSelection2));
+
+// const playerSelection1 = "scissors";
+// console.log("Player: " + playerSelection1);
+// const computerSelection1 = getComputerChoice(randomInt);
+// console.log("Computer: "+ computerSelection1);
+// console.log(playRound(playerSelection1, computerSelection1));
+
+// const playerSelection2 = "rock";
+// console.log("Player: " + playerSelection2);
+// const computerSelection2 = getComputerChoice(randomInt);
+// console.log("Computer: "+ computerSelection2);
+// console.log(playRound(playerSelection2, computerSelection2));
