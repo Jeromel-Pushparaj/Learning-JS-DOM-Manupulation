2ed5cfedcae6fc8db55593bb55a60b307b4f6768
Jeromel Pushparaj <jeromal03@gmail.com>
2024-04-22 21:14:17 +0530

    logics checks DOM is pendings in javascripts

--- Projects/rock-paper-scissors/rps.js
+++ Projects/rock-paper-scissors/rps.js
@@ -1,60 +1,29 @@
-var randomInt = Math.floor(Math.random()*3);
-console.log(randomInt);
-
-function getComputerChoice(random){
-    var choice = ['rock','papper','scissors'];
-    return choice[random];
+function getComputerChoice(){
+    var randomInt = Math.floor(Math.random()*3);
+    console.log(randomInt);
+    var choice = ['rock','papper','scissor'];
+    return choice[randomInt];
 }
-let youWin = false;
-function playerRound(playerSelection,computerSelection){
-    if (playerSelection==='rock' && computerSelection === 'scissors') {
+
+function playerRound(button){
+    let playerSelection = button.id;
+    let computerSelection = getComputerChoice();
+    let youWin = false;
+    if (playerSelection==='rock' && computerSelection === 'scissor') {
         youWin = true;
     } 
-    if(playerSelection=== 'papper' && computerSelection === 'rock'){
-        youwin = true;
+    if(playerSelection === 'papper' && computerSelection === 'rock'){
+        youWin = true;
     }
     if(playerSelection==='scissor' && computerSelection === 'papper'){
         youWin = true;
-        
     }
-    if (playerSelection!=computerSelection && computerSelection === 'scissors' && computerSelection != 'rock') {
-        var result = computerSelection + " Beats " + playerSelection;
-        return result;
-    }
-    if(playerSelection!=computerSelection && computerSelection === 'papper' && computerSelection != 'scissors'){
-        result =computerSelection + " Beats " + playerSelection;
-        return result;
-        
-    }
-    if(playerSelection!=computerSelection && computerSelection === 'rock' && computerSelection != 'papper'){
-        result =computerSelection + " Beats " + playerSelection;
-        return result;
-        
-    }
-}
+    console.log(`p:${playerSelection}: c:${computerSelection}`,youWin);
 
-const prompt = require('prompt-sync')({ sigint: true });
-let i = 0;
-while(i == 0){
-    let playerSelection = prompt("Enter you choice: ");
-    console.log("Player: " + playerSelection);
-    const computerSelection = getComputerChoice(randomInt);
-    console.log("Computer: "+ computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    i = prompt("continue = 0, exit = 1: ")
 }
 
 
-
-
-// const playerSelection1 = "scissors";
-// console.log("Player: " + playerSelection1);
-// const computerSelection1 = getComputerChoice(randomInt);
-// console.log("Computer: "+ computerSelection1);
-// console.log(playRound(playerSelection1, computerSelection1));
-
-// const playerSelection2 = "rock";
-// console.log("Player: " + playerSelection2);
-// const computerSelection2 = getComputerChoice(randomInt);
-// console.log("Computer: "+ computerSelection2);
-// console.log(playRound(playerSelection2, computerSelection2));
+var buttons = document.querySelectorAll('button');
+buttons = buttons.forEach(button => button.addEventListener('click', function() {
+    playerRound(button)
+}));
